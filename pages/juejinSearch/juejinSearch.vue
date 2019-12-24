606414b3d29000da3374217a0ba4cb4aa7098304
glorydreamtop <hanwenhui520@gmail.com>
2019-12-24 14:43:21 +0800

    新建搜索页面

---
+++ pages/juejinSearch/juejinSearch.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	import config from './config';
+	import MescrollItem from '../../components/mescroll-item';
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
